1b6be352feae5346fe09684e46ac60af220f2066
Gianluca Liccardo <gianluca_liccardo@libero.it>
2025-01-17 09:22:17 +0700

    add languages and section word

--- src/pages/GameContent.jsx
+++ src/pages/GameContent.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react"
+import { languages } from "../languages"
+
 export function GameContent(){
+
+    const [currentWord, setCurrentWorld] = useState("react");
+
+   const languageElements = languages.map(language => {
+
+    const styles = {
+        backgroundColor: language.backgroundColor,
+        color: language.color
+    }
     return (
+        <span key={language.name} className="chip" style={styles}>{language.name}</span>
+    )
+   })
+
+ const letterElements = currentWord.split('').map((letter,index) => (
+    <span key={index}>{letter.toUpperCase()}</span>
+ ))
+   
+return (
         <>
             <section className="game-status">
                <h2>You win!</h2>
-               <p>Well done!</p>
+               <p>Well done! </p>
+            </section>
+
+            <section className="language-chips"> 
+                {languageElements}
+            </section>
+
+            <section className="word">
+            {letterElements}
             </section>
         </>
     )
